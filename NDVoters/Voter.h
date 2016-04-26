f981bb61e76c23a11ccb25e8fde68724b6679655
Erin Flynn <eflynn3@nd.edu>
2016-04-26 17:11:51 -0400

    Comment Voters class and implementation

--- NDVoters/Voter.h
+++ NDVoters/Voter.h
@@ -1,3 +1,5 @@
+/*Voter class which stores information about each voter, including name of candidate whom voter is voting for, voter's age, gender, dorm, and political party */
+
 #include <iostream> 
 #include <string> 
 #ifndef VOTER_H
@@ -6,17 +8,17 @@ using namespace std;
 
 class Voter { 
 	public: 
-		Voter();
-		void setAge(string ageVal);
-		void setGender(string genderVal);
-		void setCandidate(string candidateVal ); 
-		void setDorm(string dormVal);
-		void setParty(string partyVal); 
-		string getCandidate();
-		string getGender();
-		string getDorm();
-		string getAge();
-		string getParty(); 
+		Voter(); //constructor
+		void setAge(string ageVal); //stores age for voter
+		void setGender(string genderVal); //stores gender for voter
+		void setCandidate(string candidateVal ); //stores the candidate whom voter is voting for 
+		void setDorm(string dormVal); //stores the dorm for the voter
+		void setParty(string partyVal); //stores the party of the voter
+		string getCandidate(); //returns the candidate
+		string getGender(); //returns the gender
+		string getDorm(); //returns the dorm
+		string getAge(); //returns the age
+		string getParty(); //returns the party 
 	private: 
 		string age; 
 		string gender; 
